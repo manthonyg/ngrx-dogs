2477e472a03b94a0a7c40bdf95a3c2e58a4d2efc
manthony <41161951+manthonyg@users.noreply.github.com>
2021-03-19 13:44:52 -0400

    project strcture/routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDogComponent } from './dog-feature/components/add-dog/add-dog.component';
+import { DogListComponent } from './dog-feature/components/dog-list/dog-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DogListComponent,
+  },
+  { path: 'add', component: AddDogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DogListComponent } from './dog-feature/components/dog-list/dog-list.component';
+import { AddDogComponent } from './dog-feature/components/add-dog/add-dog.component';
+import { HeaderComponent } from './header-feature/components/header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogListComponent,
+    AddDogComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

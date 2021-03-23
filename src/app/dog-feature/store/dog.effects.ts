1c663a6ad18eb084349b2bfc94c728b30534663e
manthony <41161951+manthonyg@users.noreply.github.com>
2021-03-23 19:32:53 -0400

    remove unecessary deps

--- src/app/dog-feature/store/dog.effects.ts
+++ src/app/dog-feature/store/dog.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { EMPTY, fromEventPattern, of } from 'rxjs';
+import { of } from 'rxjs';
 import { map, mergeMap, catchError } from 'rxjs/operators';
 import { DogService } from '../service/dog.service';
 import {
